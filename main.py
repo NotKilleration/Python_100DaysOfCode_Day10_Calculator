35aeb5dd99f9cded125d804449fdbe1dd79fa87d
NotKilleration <77356224+NotKilleration@users.noreply.github.com>
2023-01-21 13:24:29 +0530

    s

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #Defining all the functions
 
+
+
 def add(a, b):
   sum = a + b
   return sum
 
-def subtact(a, b):
+def subtract(a, b):
   diff = a - b
   return diff
 
@@ -31,4 +33,8 @@ operations = {
 n1 = int(input("Enter the first number: "))
 n2 = int(input("Enter the second number: "))
 
-operation_symbol = input("What operation do you want to do? Enter '+' for Addition, '-' for subtraction, '*', for Multiplication and '/' for Division")
+operation_symbol = input("What operation do you want to use? Enter '+' for Addition, '-' for subtraction, '*', for Multiplication and '/' for Division")
+
+operation = {}
+
+# operation_choice = input("Press 1 to end program /n Press 2 to continue operating with the same numbers /n Press 3 to enter new numbers.") 
